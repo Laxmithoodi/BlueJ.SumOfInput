a5f5eaf02db4f1adc2fef4b939f8ec0b97a354c9
Laxmi Thoodi <laxmithoodi@zipcoders-MacBook-Pro-3.local>
2019-02-06 08:10:10 -0500

    sumofinput lab done

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        int n;
+        //int sum=0;
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();	 
+        int sum =(n +1)*n/2;
+        
+        System.out.println("Sum of the Numbers is " +sum);
+        
 
     }
 }
